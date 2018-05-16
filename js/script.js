e7220987461d8ef9c3037781c26ab1ccbff3c63a
Daniel Rossos <d.rossos.53@gmail.com>
2018-05-15 22:56:26 -0400

    First animation test

--- js/script.js
+++ js/script.js
@@ -5,12 +5,43 @@ canvas.style.background = raceBColor;
 
 var jCanvas = $('#canvas');
 
-jCanvas.addLayer({
-	name: 'centeralPlanet',
-	type: 'ellipse',
-	fillStyle: '#FFFFFF',
-	x: canvas.width/2, y: canvas.height/2,
-	width: 100, height: 100
-})
-.drawLayers();
+var radii = [3]
+
+function Planet(name, fill, xPos, yPos, width, height,mass) {
+    this.name = name;
+    this.fill = fill;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.width = width;
+    this.height = height;
+    this.mass = mass;
+}
+
+Planet.prototype.build = function() {
+    jCanvas.addLayer({
+       name: this.name,
+       fillStyle: this.fill,
+       type: 'ellipse',
+       x: this.xPos,
+       y: this.yPos,
+       width: this.width,
+       height: this.height
+    }).drawLayers();
+};
+
+var center = new Planet("central","#FF0000", canvas.width/2, canvas.height/2, 100, 100, 50);
+center.build();
+
+var testSatlite = new Planet("satalite", "#00BAFF", canvas.width/2, canvas.height/radii[0], 30, 30, 10);
+testSatlite.build();
+
+//right now does not return to origin pos
+jCanvas.animateLayer('satalite',{
+  x:150, y:150,
+  width: 30, height: 30
+}, 1000, /*function(layer){
+  $(this).animateLayer(layer, {
+    x: canvas.width/2, y: canvas.height/radii[0]
+  }, 'slow', 'translation');
+}*/);
 
