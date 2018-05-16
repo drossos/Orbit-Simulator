105ad6bba52a00c07284aaf5aa071aba51816009
drossos <drossos@bayviewglen.ca>
2018-05-16 00:07:41 -0400

    circular kinda??

--- js/script.js
+++ js/script.js
@@ -1,47 +1,60 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext("2d");
-var raceBColor = "green"
+var raceBColor = "green";
 canvas.style.background = raceBColor;
-
+var angle = 80;
+var speed = 5;
 var jCanvas = $('#canvas');
 
 var radii = [3]
 
-function Planet(name, fill, xPos, yPos, width, height,mass) {
+function Planet(name, fill, xPos, yPos, radius, mass) {
     this.name = name;
     this.fill = fill;
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.width = width;
-    this.height = height;
+    this.x = xPos;
+    this.y = yPos;
+    this.radius = radius;
     this.mass = mass;
 }
 
 Planet.prototype.build = function() {
     jCanvas.addLayer({
-       name: this.name,
-       fillStyle: this.fill,
-       type: 'ellipse',
-       x: this.xPos,
-       y: this.yPos,
-       width: this.width,
-       height: this.height
+        name: this.name,
+        fillStyle: this.fill,
+        type: 'ellipse',
+        x: this.x,
+        y: this.y,
+        width: this.radius,
+        height: this.radius
     }).drawLayers();
 };
 
-var center = new Planet("central","#FF0000", canvas.width/2, canvas.height/2, 100, 100, 50);
+var center = new Planet("central", "#FF0000", canvas.width / 2, canvas.height / 2, 100, 50);
 center.build();
 
-var testSatlite = new Planet("satalite", "#00BAFF", canvas.width/2, canvas.height/radii[0], 30, 30, 10);
+var testSatlite = new Planet("satalite", "#00BAFF", canvas.width / 2, canvas.height / radii[0], 30, 10);
 testSatlite.build();
+function animate() {
 
+var testX = center.x - (getRadius(testSatlite) * Math.cos(70));
+var testY = center.y + (getRadius(testSatlite) * Math.sin(70));
+testSatlite.x = testX;
+testSatlite.y = testY;
 //right now does not return to origin pos
-jCanvas.animateLayer('satalite',{
-  x:150, y:150,
-  width: 30, height: 30
-}, 1000, /*function(layer){
-  $(this).animateLayer(layer, {
-    x: canvas.width/2, y: canvas.height/radii[0]
-  }, 'slow', 'translation');
-}*/);
 
+    jCanvas.animateLayer('satalite', {
+        x: testX,
+        y: testY,
+        width: 30,
+        height: 30
+    }, 1000);
+
+    angle-= speed;
+}
+
+function getRadius(planet) {
+    var test = Math.sin(30);
+    return Math.sqrt(Math.pow(Math.abs(planet.x - center.x), 2) + Math.pow(Math.abs(planet.y - center.y), 2));
+}
+
+setInterval(animate, 100);
